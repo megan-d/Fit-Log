83e7c5ac06a8ca8c88dded72d1ccd5dadb8e7de7
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-05 21:28:15 -0600

    Clean up and modify Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/Logo.svg';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    //Update so that you can use isSignedIn and based on that value update the navigation
     return(
         <header>
-      <div class="logo-container">
+      <div className="logo-container">
         <img src={Logo} alt="logo" id="logo-image"/>
         <h1><a href="#">Fit Ally</a></h1>
       </div>
 
       <nav>
-        <ul class="nav-links">
-          <li><Link class="nav-link" to="/demo">View Demo</Link></li>
-          <li><Link class="nav-link" to="/register">Sign Up</Link></li>
-          <li><Link class="nav-link" to="/login">Login</Link></li>
+        <ul className="nav-links">
+          <li><Link className="nav-link" to="/demo">View Demo</Link></li>
+          <li><Link className="nav-link" to="/register">Sign Up</Link></li>
+          <li><Link className="nav-link" to="/login">Login</Link></li>
         </ul>
       </nav>
     </header>
