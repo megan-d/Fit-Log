39dd688ab69dfd253637764e4b57e5b7240cfdc7
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-07 14:11:41 -0600

    Build register and contact pages. Move Layout component back to App. Minor styling modifications.

---
+++ client/src/containers/auth/Register.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/layout/Navbar';
+import Footer from '../../components/layout/Footer';
+
+const Register = () => {
+  return (
+        <div className='main-content'>
+          <div className='main-section-signup'>
+            <h1 className='title-white-bold'>
+              Get <span className='title-orange'>FIT</span> with us.
+            </h1>
+
+            <form className='form-right' action=''>
+              <div className='form-container-signup'>
+                <div className='form-group'>
+                  <label>
+                    Name:
+                    <input type='text' name='name' placeholder='' required />
+                  </label>
+                </div>
+                <div className='form-group'>
+                  <label>
+                    Email:
+                    <input type='email' name='email' placeholder='' required />
+                  </label>
+                </div>
+                <div className='form-group'>
+                  <label>
+                    Password:
+                    <input
+                      type='password'
+                      name='password'
+                      placeholder=''
+                      required
+                    />
+                  </label>
+                </div>
+                <div className='form-group'>
+                  <label>
+                    Confirm Password:
+                    <input
+                      type='password'
+                      name='confirm'
+                      placeholder=''
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
+              <input
+                type='submit'
+                value='Sign Up'
+                className='button form-button'
+              />
+            </form>
+          </div>
+
+          <div className='have-account'>
+            <p>Already have an account?</p>
+            <Link to='/login'>Log In</Link>
+          </div>
+        </div>
+  );
+};
+
+export default Register;
--- client/src/views/Error.js
+++ client/src/views/Error.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Error= () => {
   return (
+    <div className="body-grow">
+
     <h1>Page Not Found</h1>
+    </div>
   );
 };
 
