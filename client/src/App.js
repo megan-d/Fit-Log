eabec36d936c40f797d1c23f74dea4133a97845a
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-06 20:54:30 -0600

    Build login container and minor modifications to styles

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './views/About';
 import Landing from './views/Landing';
+import Login from './containers/auth/Login';
 import Error from './views/Error';
 
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
           <Switch >
             <Route path="/" component={Landing} exact />
             <Route path="/about" component={About} exact />
+            <Route path="/login" component={Login} exact />
             <Route path="" component={Error} />
           </Switch>
     </Router>
