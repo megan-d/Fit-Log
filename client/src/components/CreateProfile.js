2049d4435390db6c5abd8b09a64583a72283303f
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-09 14:08:07 -0600

    Continue to build CreateProfile component

--- client/src/components/CreateProfile.js
+++ client/src/components/CreateProfile.js
@@ -4,31 +4,35 @@ const CreateProfile = () => {
   return (
         <div className='main-content'>
           <div className="form-page-container">
-            <h1 className="title-white">Contact Us</h1>
+            <h1 className="title-white">Create Your Fitness Profile</h1>
         <form className="form contact-form" action="">
           <div className="form-container">
             <div className="form-group">
-              <label>Name:
-              <input type="text" name="name" placeholder="" required />
+              <label>What is your current weight in pounds?
+              <input type="number" name="weight" placeholder="" required />
               </label>
             </div>
             <div className="form-group">
-              <label>Email:
-              <input type="email" name="email" placeholder="" required />
+              <label>What is your height in inches?
+              <input type="number" name="height" placeholder="" required />
               </label>
             </div>
             <div className="form-group">
-              <label>Message:
-              <textarea
-                name="message"
-                rows="10"
-                cols="33"
-                className="form-message"
-                placeholder=""
-                required
-              />
+              <label>What is your goal weight in pounds?
+              <input type="number" name="goal-weight" placeholder="" required />
               </label>
             </div>
+            <div className="form-group">
+              <label>What is your daily calorie goal? If unsure, 2000 is a starting point.
+              <input type="number" name="calories" placeholder="" required />
+              </label>
+            </div>
+            <div className="form-group">
+              <label>How many days per week is your exercise goal?
+              <input type="number" name="days" placeholder="" required />
+              </label>
+            </div>
+            
           </div>
           <input type="submit" value="Submit" className="button form-button" />
         </form>
