b6e7cb2a1466a235d5c210e67d38c9a9e606349e
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-08 21:10:45 -0600

    Complete profile schema draft

--- api/models/Profile.js
+++ api/models/Profile.js
@@ -2,24 +2,63 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     min: 8
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now,
-//   },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  weight: {
+      type: Number,
+      required: true
+  },
+  height: {
+      type: Number,
+      required: true
+  },
+  activities: [
+      {
+          date: {
+              type: Date,
+              default: Date.now
+          },
+          duration: {
+              type: Number,
+              required: true
+          },
+          category: {
+              type: String,
+              required: true
+          },
+          calories: {
+              type: Number,
+          },
+          comments: {
+              type: String
+          }
+      }
+  ],
+  goals: [
+      {
+          goalWeight: {
+              type: Number,
+          },
+          days: {
+              type: Number,
+              default: 4
+          },
+          calories: {
+              type: Number,
+              default: 2000
+          }
+      }
+  ],
+  calories: [
+      {
+          consumed: {
+              type: Number
+          }
+      }
+  ]
+
 });
 
 module.exports = Profile = mongoose.model('Profile', profileSchema);
