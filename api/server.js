fbc4aa8d3a779fb6b5af22d0ce92010cd9c19fcb
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-11 20:48:16 -0600

    Add post route to create or update profile

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ const app = express();
 const connectDatabase = async () => {
     try {await mongoose.connect(
         process.env.MONGO_URI,
-        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false },
         () => console.log('Connected to DB'))
     } catch(err) {
         console.error(err);
