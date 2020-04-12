3851e6ade83f6b38d9721dc49897ea70c319427c
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-11 21:52:13 -0600

    Begin to build route to add activity to profile

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -11,143 +11,168 @@ const Profile = require('../models/Profile');
 //ROUTE: GET api/profile/me
 //DESCRIPTION: Get current user's profile
 //ACCESS LEVEL: Private
-router.get('/me', verify, async(req, res) => {
-    
-    try {
-        //Find the current user based on the id that comes in with the request's token. Populate with the name from the user model.
-        const profile = await (await Profile.findOne({ user: req.user.id })).populate('User', 'name');
-        console.log(req.user.id);
-        //If there is no profile, return an error
-        if(!profile) {
-            return res.status(400).json({ msg: "There is no profile available for this user." })
-        }
-        //If there is a profile, send that profile
-        res.json(profile);
-
-    } catch(err) {
-        console.log(err.message);
-        res.status(500).send('Server Error');
+router.get('/me', verify, async (req, res) => {
+  try {
+    //Find the current user based on the id that comes in with the request's token. Populate with the name from the user model.
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populate('User', 'name');
+    console.log(req.user.id);
+    //If there is no profile, return an error
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile available for this user.' });
     }
+    //If there is a profile, send that profile
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
-
-
 
 //ROUTE: POST api/profile
 //DESCRIPTION: Create or update a user profile
 //ACCESS LEVEL: Private
-router.post('/', [ verify, [ 
-    //User express validator to validate required inputs
-    check('weight', 'Please provide a numeric weight in pounds.').not().isEmpty(),
-    check('height', 'Please provide a numeric height in inches.').not().isEmpty(),
+router.post(
+  '/',
+  [
+    verify,
+    [
+      //User express validator to validate required inputs
+      check('weight', 'Please provide a numeric weight in pounds.')
+        .not()
+        .isEmpty(),
+      check('height', 'Please provide a numeric height in inches.')
+        .not()
+        .isEmpty()
     ]
-], async (req, res) => {
+  ],
+  async (req, res) => {
     //Add in logic for express validator error check
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
 
     //Pull all of the fields out into variables from req.body. Don't include activities.
     const {
-        weight,
-        height,
-        goalWeight,
-        goalDailyCalories,
-        goalDays,
-        caloriesConsumedToday,
-        caloriesRemainingToday
+      weight,
+      height,
+      goalWeight,
+      goalDailyCalories,
+      goalDays,
+      caloriesConsumedToday,
+      caloriesRemainingToday
     } = req.body;
 
     //Build the profileItems object. If the value is there, add it to the profileItems object.
     const profileItems = {};
     profileItems.user = req.user.id;
-    if(weight) {
-        profileItems.weight = weight;
+    if (weight) {
+      profileItems.weight = weight;
     }
-    if(height) {
-        profileItems.height = height;
+    if (height) {
+      profileItems.height = height;
     }
-    if(weight) {
-        //need to see how to make this work when updating
-        profileItems.bmi = ((weight / (height * height)) * 703).toFixed(1);
+    if (weight) {
+      //need to see how to make this work when updating
+      profileItems.bmi = ((weight / (height * height)) * 703).toFixed(1);
     }
-    if(goalWeight) {
-        profileItems.goalWeight = goalWeight;
+    if (goalWeight) {
+      profileItems.goalWeight = goalWeight;
     }
-    if(goalDailyCalories) {
-        profileItems.goalDailyCalories = goalDailyCalories;
+    if (goalDailyCalories) {
+      profileItems.goalDailyCalories = goalDailyCalories;
     }
-    if(goalDays) {
-        profileItems.goalDays = goalDays;
+    if (goalDays) {
+      profileItems.goalDays = goalDays;
     }
     //Might need to move these somewhere else - unsure yet
-    if(caloriesConsumedToday > 0) {
-        profileItems.caloriesConsumedToday = caloriesConsumedToday;
+    if (caloriesConsumedToday > 0) {
+      profileItems.caloriesConsumedToday = caloriesConsumedToday;
     }
-    if(goalDailyCalories && caloriesConsumedToday) {
-        profileItems.caloriesRemainingToday = caloriesRemainingToday;
+    if (goalDailyCalories && caloriesConsumedToday) {
+      profileItems.caloriesRemainingToday = caloriesRemainingToday;
     }
-    
+
     //Once all fields are prepared, update and populate the data
     try {
-        //Use findOne to find profile
-        let profile = await Profile.findOne({ user: req.user.id });
+      //Use findOne to find profile
+      let profile = await Profile.findOne({ user: req.user.id });
 
-        //If profile is found, update the new fields
-        if(profile) {
-            profile = await Profile.findOneAndUpdate(
-                {user: req.user.id},
-                { $set: profileItems },
-                { new: true },
-            )
-            //send back profile
-            return res.json(profile);
-        }
-        //If profile isn't found, create a new one
-        if(!profile) {
-            profile = await new Profile(profileItems);
-            await profile.save();
-            res.json(profile);
-;        }
-    } catch(err) {
-        console.error(err);
-        res.status(500).send('Server Error');
+      //If profile is found, update the new fields
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileItems },
+          { new: true }
+        );
+        //send back profile
+        return res.json(profile);
+      }
+      //If profile isn't found, create a new one
+      if (!profile) {
+        profile = await new Profile(profileItems);
+        await profile.save();
+        res.json(profile);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
     }
-}
-)
-
+  }
+);
 
 //ROUTE: DELETE api/profile
 //DESCRIPTION: Delete profile and user
 //ACCESS LEVEL: Private
-router.delete('/', verify, async(req, res) => {
-    try{
-        //Find profile that corresponds to user id found in token and delete
-        await Profile.findOneAndRemove({ user: req.user.id });
+router.delete('/', verify, async (req, res) => {
+  try {
+    //Find profile that corresponds to user id found in token and delete
+    await Profile.findOneAndRemove({ user: req.user.id });
 
-        //Find user that corresponds to user id found in token and delete
-        await User.findOneAndRemove({ _id: req.user.id });
+    //Find user that corresponds to user id found in token and delete
+    await User.findOneAndRemove({ _id: req.user.id });
 
-        res.json({ msg: 'This user and corresponding profile has been deleted.' });
-
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-})
-
+    res.json({ msg: 'This user and corresponding profile has been deleted.' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //ROUTE: PUT api/profile/activity
 //DESCRIPTION: Add activity
 //ACCESS LEVEL: Private
+router.put('/activity', [verify, [
+    //User express validator to validate required inputs
+    check('duration', 'Please provide a duration in minutes.')
+    .not()
+    .isEmpty(),
+  check('category', 'Please select a category.')
+    .not()
+    .isEmpty()
+]], async (req, res) => {
+  //Add in logic for express validator error check
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  //Pull all of the fields out into variables from req.body. 
+  const {
+    date,
+    duration,
+    category,
+    calories,
+    comments
+  } = req.body;
 
-
+});
 
 //ROUTE: DELETE api/profile/activity/:activity_id
 //DESCRIPTION: Delete activity
 //ACCESS LEVEL: Private
-
-
-
 
 module.exports = router;
