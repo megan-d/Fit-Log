c488e2c2ea18dd8481461bdf7a3faeecf0595168
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-17 14:13:52 -0600

    Update error page styling. Move views folder to within components.

---
+++ client/src/components/views/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className='body-grow'>
+      <h1 className='error-page'>Page Not Found</h1>
+    </div>
+  );
+};
+
+export default Error;
