8e934ef5fb93acfc863416a777becab962a91c5f
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-21 22:45:06 -0600

    Continue styling dashboard

--- client/src/containers/dashboard/DashboardContainer.js
+++ client/src/containers/dashboard/DashboardContainer.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 export default class DashboardContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoading: true,
+      isAuthenticated: null,
+      user: null,
+      // weight: '',
+      // height: '',
+      // bmi: '',
+      // goalWeight: '',
+      // goalDailyCalories: '',
+      // goalDays: '',
+      // caloriesConsumedToday: '',
+      // caloriesRemainingToday: '',
+      // activities: [],
+    };
+  }
+
+  componentDidMount() {
+    //Fet the user data here and set the state with the user data
+    this.setState({
+      color: 'red',
+    });
   }
 
   render() {
@@ -16,15 +36,15 @@ export default class DashboardContainer extends Component {
               <h2 className='card-title'>Stats</h2>
               <div className='card-stats'>
                 <div className='card-item'>
-                  <p className='card-label'>Weight</p>
+                  <p className='card-label'>Weight:</p>
                   <p className='card-value'>110 lbs</p>
                 </div>
                 <div className='card-item'>
-                  <p className='card-label'>Height</p>
+                  <p className='card-label'>Height:</p>
                   <p className='card-value'>64 in</p>
                 </div>
                 <div className='card-item'>
-                  <p className='card-label'>BMI</p>
+                  <p className='card-label'>BMI:</p>
                   <p className='card-value'>18.9</p>
                 </div>
               </div>
@@ -32,9 +52,42 @@ export default class DashboardContainer extends Component {
             </div>
             <div className='card'>
               <h2 className='card-title'>Goals</h2>
+              <div className='card-stats'>
+                <div className='card-item'>
+                  <p className='card-label'>Weight:</p>
+                  <p className='card-value'>100 lbs</p>
+                </div>
+                <div className='card-item'>
+                  <p className='card-label'>Daily Calories:</p>
+                  <p className='card-value'>2000</p>
+                </div>
+                <div className='card-item'>
+                  <p className='card-label'>Days/Week Exercise:</p>
+                  <p className='card-value'>4</p>
+                </div>
+              </div>
+              <button className='card-button'>Update</button>
             </div>
             <div className='card'>
-              <h2 className='card-title'>Daily Calorie Tracker</h2>
+              <h2 className='card-title'>Daily Calories Tracker</h2>
+              <div className='card-stats'>
+                <div className='card-item'>
+                  <p className='card-label'>Calories Consumed:</p>
+                  <p className='card-value'>600</p>
+                </div>
+                <div className='card-item'>
+                  <p className='card-label'>Calories Remaining:</p>
+                  <p className='card-value'>1400</p>
+                </div>
+              </div>
+              <div className='card-item calories-input'>
+                <input
+                  className='card-input'
+                  type='number'
+                  name='add-calories'
+                ></input>
+                <button className='card-button'>Add Calories</button>
+              </div>
             </div>
           </div>
         </div>
