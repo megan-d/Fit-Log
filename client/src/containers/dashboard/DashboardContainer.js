1451ce4a78327403383a4571963541eee4351ece
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-22 22:26:38 -0600

    Refactor to make DailyCaloriesCard a separate component which calls database for the calories data and then updates that data based on input.

--- client/src/containers/dashboard/DashboardContainer.js
+++ client/src/containers/dashboard/DashboardContainer.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import DailyCaloriesCard from '../../components/DailyCaloriesCard';
 
 export default class DashboardContainer extends Component {
   constructor(props) {
@@ -16,14 +17,10 @@ export default class DashboardContainer extends Component {
         goalWeight: null,
         goalDailyCalories: 0,
         goalDays: 0,
-        caloriesConsumedToday: 0,
-        caloriesRemainingToday: 0,
         activities: [],
       },
       weightInput: 0,
     };
-    this.addCalories.bind(this);
-    this.sendToDatabase.bind(this);
   }
 
   //Fetch the user data here and set the state with the user data (api request to /api/profile/me). The api response will be the profile object, which can then be added to state.
@@ -48,66 +45,10 @@ export default class DashboardContainer extends Component {
         goalWeight: profile.data.goalWeight,
         goalDailyCalories: profile.data.goalDailyCalories,
         goalDays: profile.data.goalDays,
-        caloriesConsumedToday: profile.data.caloriesConsumedToday,
-        caloriesRemainingToday: profile.data.caloriesRemainingToday,
         activities: profile.data.activities,
       },
     });
-    console.log(this.state);
   }
-
-  inputCalories = (e) => {
-    this.setState({
-      weightInput:
-        e.target.type === 'number' ? parseInt(e.target.value) : e.target.value,
-    });
-  };
-
-  addCalories = () => {
-    //When button is clicked, get current value from state for weightInput and add to current value of state for caloriesConsumedToday. Update state for caloriesConsumedToday and caloriesRemaining today.
-    if (this.state.profile.caloriesRemainingToday > 0) {
-      this.setState((prevState) => ({
-        profile: {
-          ...prevState.profile,
-          caloriesConsumedToday:
-            prevState.profile.caloriesConsumedToday + this.state.weightInput,
-          caloriesRemainingToday:
-            prevState.profile.caloriesRemainingToday - this.state.weightInput,
-        },
-      }));
-    } else {
-      this.setState((prevState) => ({
-        profile: {
-          ...prevState.profile,
-          caloriesConsumedToday:
-            prevState.profile.caloriesConsumedToday + this.state.weightInput,
-          caloriesRemainingToday: 0,
-        },
-      }));
-    }
-  };
-
-  sendToDatabase = async () => {
-    //Update the caloriesConsumedToday and caloriesRemainingToday in database - need to figure out how to do this to get updated state information sent
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': localStorage.token,
-      },
-    };
-    const calories = {
-      caloriesConsumedToday: this.state.caloriesConsumedToday,
-      caloriesRemainingToday: this.state.caloriesRemainingToday,
-    };
-
-    const body = JSON.stringify(calories);
-
-    try {
-      await axios.put('/api/profile', body, config);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   render() {
     return (
@@ -165,40 +106,7 @@ export default class DashboardContainer extends Component {
                   Update
                 </Link>
               </div>
-              <div className='card'>
-                <h2 className='card-title'>Daily Calories Tracker</h2>
-                <div className='card-stats'>
-                  <div className='card-item'>
-                    <p className='card-label'>Calories Consumed:</p>
-                    <p className='card-value'>
-                      {this.state.profile.caloriesConsumedToday}
-                    </p>
-                  </div>
-                  <div className='card-item'>
-                    <p className='card-label'>Calories Remaining:</p>
-                    <p className='card-value'>
-                      {this.state.profile.caloriesRemainingToday < 0
-                        ? 0
-                        : this.state.profile.caloriesRemainingToday}
-                    </p>
-                  </div>
-                </div>
-                <div className='card-item calories-input'>
-                  <input
-                    className='card-input'
-                    type='number'
-                    name='weightInput'
-                    value={this.state.weightInput}
-                    onChange={(e) => this.inputCalories(e)}
-                  ></input>
-                  <button
-                    className='card-button'
-                    onClick={() => this.addCalories()}
-                  >
-                    Add Calories
-                  </button>
-                </div>
-              </div>
+              <DailyCaloriesCard />
             </div>
           </div>
         </div>
@@ -206,6 +114,9 @@ export default class DashboardContainer extends Component {
     );
   }
 }
+
+
+
 
 // {/* <div className='charts'>
 //             <div className='chart'>Chart 1</div>
