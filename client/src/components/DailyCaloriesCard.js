c7744a723656b33a3463f591613db7bda7e1a1c4
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-23 22:09:43 -0600

    Remove useEffect import and create file for Spinner component.

--- client/src/components/DailyCaloriesCard.js
+++ client/src/components/DailyCaloriesCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const DailyCaloriesCard = (props) => {
   const [calorieData, updateCalorieData] = useState({
