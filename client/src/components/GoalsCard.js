3daf17ee76c17c7db2d1798ddda6a96873e3e598
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-24 13:40:16 -0600

    Put goals in separate component named GoalsCard and bring into DashboardContainer

---
+++ client/src/components/GoalsCard.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const GoalsCard = (props) => {
+  return (
+    <div className='card'>
+      <h2 className='card-title'>Goals</h2>
+      <div className='card-stats'>
+        <div className='card-item'>
+          <p className='card-label'>Weight:</p>
+          <p className='card-value'>
+            {props.goalWeight
+              ? props.goalWeight
+              : '-'}{' '}
+            lbs
+          </p>
+        </div>
+        <div className='card-item'>
+          <p className='card-label'>Daily Calories:</p>
+          <p className='card-value'>{props.goalDailyCalories}</p>
+        </div>
+        <div className='card-item'>
+          <p className='card-label'>Days/Week Exercise:</p>
+          <p className='card-value'>{props.goalDays}</p>
+        </div>
+      </div>
+      <Link className='card-button' to='/stats'>
+        Update
+      </Link>
+    </div>
+  );
+};
+
+export default GoalsCard;
