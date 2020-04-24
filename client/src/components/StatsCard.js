9fe6c21ea23bf00640d31626dcd2b60fa7d4d241
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-24 13:54:37 -0600

    Refactor to add cards into Cards component located within DashboardContainer

---
+++ client/src/components/StatsCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const StatsCard = (props) => {
+  return (
+    <div className='card'>
+      <h2 className='card-title'>Stats</h2>
+      <div className='card-stats'>
+        <div className='card-item'>
+          <p className='card-label'>Weight:</p>
+          <p className='card-value'>{props.weight} lbs</p>
+        </div>
+        <div className='card-item'>
+          <p className='card-label'>Height:</p>
+          <p className='card-value'>{props.height} in</p>
+        </div>
+        <div className='card-item'>
+          <p className='card-label'>BMI:</p>
+          <p className='card-value'>{props.bmi}</p>
+        </div>
+      </div>
+      <Link className='card-button' to='/stats'>
+        Update
+      </Link>
+    </div>
+  );
+};
+
+export default StatsCard;
