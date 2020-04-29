0c62ca5386e7e713221d8a7b0e0e6c47002c7b45
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-28 22:06:02 -0600

    Begin to build Redux functionality for auth and flash alerts.

---
+++ client/src/actions/alert.js
@@ -0,0 +1,6 @@
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+//Want to be able to dispatch more than one action type from this function. Can add dispatch to do this. Able to do this because of Thunk middleware.
+export const setAlert = (message, alertType) => dispatch => {
+
+}
