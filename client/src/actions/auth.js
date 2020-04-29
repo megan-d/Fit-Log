5ea0c1a4c77f04722f95db7c480b3a4d6fb5e989
Megan D <45672063+megan-d@users.noreply.github.com>
2020-04-28 21:30:53 -0600

    Add note and begin register action

---
+++ client/src/actions/auth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { REGISTER_BEGIN, REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+
+
+//****LOAD USER ACTION */
+
+
+//****REGISTER USER ACTION */
+//name is props.name, email is props.email, password is props.password
+export const register = (props) => async dispatch => {
+
+}
--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
+//Initial state here will be empty object. All initial state will be in the reducers.
 const initialState = {};
 
 const store = createStore(
