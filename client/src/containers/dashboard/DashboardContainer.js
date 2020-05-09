990520d8b758d8cd6dcb4771ea26f83ce90e368e
Megan D <45672063+megan-d@users.noreply.github.com>
2020-05-09 15:25:44 -0600

    Fix spinner placement on dashboard

--- client/src/containers/dashboard/DashboardContainer.js
+++ client/src/containers/dashboard/DashboardContainer.js
@@ -13,31 +13,32 @@ const DashboardContainer = ({ getCurrentUserProfile, auth, profile }) => {
     getCurrentUserProfile();
   }, [getCurrentUserProfile]);
 
-
-  return profile.loading && profile.profile === null? 
-  (
-    <Spinner />
-  ) :
-  (
+  return profile.loading && profile.profile === null ? (
+    <div className='main-content'>
+      <div className='dashboard-container'>
+        <h1 className='title-white-bold'>Your Dashboard</h1>
+        <Spinner />
+      </div>
+    </div>
+  ) : (
     <Fragment>
       <div className='main-content'>
         <div className='dashboard-container'>
           <h1 className='title-white-bold'>Your Dashboard</h1>
           {profile.profile !== null ? (
-            <Fragment >
+            <Fragment>
               <Cards profile={profile.profile} />
-            <Charts />
+              <Charts />
             </Fragment>
-          ) : 
-          (
-            <Fragment >
-              <p>You do not have a profile set up. Please add this information to view your dashboard.
+          ) : (
+            <Fragment>
+              <p>
+                You do not have a profile set up. Please add this information to
+                view your dashboard.
                 <Link to='/createprofile'>Create Profile</Link>
               </p>
             </Fragment>
-          )
-          }
-          
+          )}
         </div>
       </div>
     </Fragment>
@@ -48,11 +49,13 @@ DashboardContainer.propTypes = {
   getCurrentUserProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
-}
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile,
-})
+});
 
-export default connect(mapStateToProps, { getCurrentUserProfile })(DashboardContainer);
+export default connect(mapStateToProps, { getCurrentUserProfile })(
+  DashboardContainer,
+);
