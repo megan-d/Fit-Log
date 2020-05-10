6c6880970806ccc5a29586ee9062919295a5dbbf
Megan D <45672063+megan-d@users.noreply.github.com>
2020-05-10 15:34:57 -0600

    Continue to integrate redux

--- client/src/components/dashboard/CreateProfile.js
+++ client/src/components/dashboard/CreateProfile.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createProfile } from '../actions/profile';
+import { createProfile } from '../../actions/profile';
 
 const CreateProfile = ({ createProfile, history }) => {
   const [formData, updateFormData] = useState({
