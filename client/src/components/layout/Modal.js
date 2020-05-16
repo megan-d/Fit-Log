9738903cb21ed99e02cf7cc1e399d284f42f9713
Megan D <45672063+megan-d@users.noreply.github.com>
2020-05-16 14:20:33 -0600

    Add modal backdrop

--- client/src/components/layout/Modal.js
+++ client/src/components/layout/Modal.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Backdrop from '../layout/Backdrop';
 
-const Modal = ({ show, children }) => (
-  <div className={show ? 'modal-on' : 'modal-off'}>
-    <div className='modal'>{children}</div>
-  </div>
+const Modal = ({ show, children, modalClosed }) => (
+  <Fragment>
+    <Backdrop  show={show} clicked={modalClosed} />
+    <div className={show ? 'modal-on' : 'modal-off'}>
+      <div className='modal'>{children}</div>
+    </div>
+  </Fragment>
 );
 
 export default Modal;
