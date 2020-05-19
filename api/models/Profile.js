080accf6eb7df756ad82ae6e6e4150368a7676d5
Megan D <45672063+megan-d@users.noreply.github.com>
2020-05-19 14:27:51 -0600

    Add weight tracker line graph and add weightHistory to Profile model and profile route

--- api/models/Profile.js
+++ api/models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
-     //Will need to address scenario where certain data isn't provided (either make all required or account for those scenarios in logic)
+  //Will need to address scenario where certain data isn't provided (either make all required or account for those scenarios in logic)
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
@@ -11,32 +11,43 @@ const profileSchema = new Schema({
     type: Number,
     required: true,
   },
+  weightHistory: [
+    {
+      weight: {
+        type: Number,
+      },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+  ],
   height: {
     type: Number,
     required: true,
   },
   bmi: {
     type: Number,
-},
+  },
   goalWeight: {
     type: Number,
   },
   goalDailyCalories: {
     type: Number,
-    default: 2000
+    default: 2000,
   },
   goalDays: {
     type: Number,
-    default: 4
+    default: 4,
   },
   caloriesConsumedToday: {
     //This will need to reset every day
     type: Number,
-    default: 0
+    default: 0,
   },
   caloriesRemainingToday: {
     type: Number,
-},
+  },
   activities: [
     {
       date: {
@@ -55,7 +66,7 @@ const profileSchema = new Schema({
         type: Number,
       },
     },
-  ]
+  ],
 });
 
 module.exports = Profile = mongoose.model('Profile', profileSchema);
