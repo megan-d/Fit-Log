825b16d5d9bc543a78d67a8a200e21f945f0b82d
Megan D <45672063+megan-d@users.noreply.github.com>
2020-05-24 14:25:34 -0600

    Adjust mobile nav

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import SideDrawer from './SideDrawer';
 
 const Layout = (props) => {
   return (
     <div className="body-grow">
       <Navbar />
-      <SideDrawer />
       {props.children}
       <Footer />
     </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,12 +6,16 @@ import { logoutUser } from '../../actions/auth';
 import Logo from '../../assets/images/Logo.svg';
 
 const Navbar = ({ logoutUser, auth: { isAuthenticated, isLoading } }) => {
-
-const [ isOpen, toggle ] = useState(false);
+  const [isOpen, toggle] = useState(false);
 
   const toggleNav = () => toggle(!isOpen);
 
   const navClass = isOpen ? 'nav-links open' : 'nav-links close';
+
+  const logoutUserNav = () => {
+    toggleNav();
+    logoutUser();
+  }
 
   return (
     <header>
@@ -24,48 +28,62 @@ const [ isOpen, toggle ] = useState(false);
 
       <nav>
         {!isLoading && (
-          <Fragment>{isAuthenticated ? <Fragment>
-            <div className='hamburger' onClick={toggleNav}>
-              <div className='line line1'></div>
-              <div className='line line2'></div>
-              <div className='line line3'></div>
-            </div>
-            <ul className={navClass}>
-              <li>
-                <Link className='nav-link' to='/dashboard' onClick={toggleNav}>
-                  My Dashboard
-                </Link>
-              </li>
-              <li>
-                <a className='nav-link' onClick={logoutUser} href='/'>
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </Fragment> : <Fragment>
-      <div className='hamburger' onClick={toggleNav}>
-        <div className='line'></div>
-        <div className='line'></div>
-        <div className='line'></div>
-      </div>
-      <ul className={navClass}>
-        <li>
-          <Link className='nav-link' to='/demo' onClick={toggleNav}>
-            View Demo
-          </Link>
-        </li>
-        <li>
-          <Link className='nav-link' to='/register' onClick={toggleNav}>
-            Sign Up
-          </Link>
-        </li>
-        <li>
-          <Link className='nav-link' to='/login' onClick={toggleNav}>
-            Login
-          </Link>
-        </li>
-      </ul>
-    </Fragment>}</Fragment>
+          <Fragment>
+            {isAuthenticated ? (
+              <Fragment>
+                <div className='hamburger' onClick={toggleNav}>
+                  <div className='line line1'></div>
+                  <div className='line line2'></div>
+                  <div className='line line3'></div>
+                </div>
+                <ul className={navClass}>
+                  <li>
+                    <Link
+                      className='nav-link'
+                      to='/dashboard'
+                      onClick={toggleNav}
+                    >
+                      My Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <a className='nav-link' onClick={logoutUserNav} href='/'>
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <div className='hamburger' onClick={toggleNav}>
+                  <div className='line'></div>
+                  <div className='line'></div>
+                  <div className='line'></div>
+                </div>
+                <ul className={navClass}>
+                  <li>
+                    <Link className='nav-link' to='/demo' onClick={toggleNav}>
+                      View Demo
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className='nav-link'
+                      to='/register'
+                      onClick={toggleNav}
+                    >
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className='nav-link' to='/login' onClick={toggleNav}>
+                      Login
+                    </Link>
+                  </li>
+                </ul>
+              </Fragment>
+            )}
+          </Fragment>
         )}
       </nav>
     </header>
