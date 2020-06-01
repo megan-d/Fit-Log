270967ae91c85b0a17d0caf0653b78f5624c3893
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-01 14:32:57 -0600

    Continue to update site styling

--- client/src/components/views/Landing.js
+++ client/src/components/views/Landing.js
@@ -5,16 +5,27 @@ import Line from '../../assets/images/Line.svg';
 const Landing = () => {
   //Need to figure out how to not display footer on landing page
   return (
-      <div className='main-content'>
+    <div className='main-content'>
       <section className='main-section'>
         <div className='left-wrapper'>
-          <h1 className='title-landing'>Visualize Your <span className='fitness'>Fitness</span></h1>
+          <h1 className='title-landing'>
+            Visualize Your <span className='fitness'>Fitness</span>
+          </h1>
           <h2 className='subtitle-landing'>
-            Activity and fitness tracking to help you view your progress and reach your fitness goals
+            Activity and fitness tracking to help you view your progress and
+            reach your fitness goals
           </h2>
-          <a href='/register' className='button'>
-            Join Now
-          </a>
+          <div className='landing-buttons'>
+            <div className='button landing-button'>
+              <a href='/demo' className='landing-link'>Demo</a>
+            </div>
+            <div className='button landing-button'>
+              <a href='/register' className='landing-link'>
+                Join Now
+              </a>
+            </div>
+          </div>
+
           <img className='image-line' src={Line} alt='line' />
         </div>
 
@@ -23,7 +34,6 @@ const Landing = () => {
         </div>
       </section>
     </div>
-    
   );
 };
 
