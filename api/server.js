8e31ccc1bafb0b083a879b6b837fb27e0a8b390f
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-04 14:39:26 -0600

    Add heroku postbuild script

--- api/server.js
+++ api/server.js
@@ -49,7 +49,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-  
+    //Return all requests to react app
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
