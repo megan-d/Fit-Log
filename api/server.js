f6d24b8e2cdf6f689812bae4d793e8d2803278e5
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-04 16:02:01 -0600

    Update postbuild to try to fix deploy

--- api/server.js
+++ api/server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./client/build'));
     //Return all requests to react app
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '/client', 'build', 'index.html'));
     });
   }
 
