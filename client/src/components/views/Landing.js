03e8e3a40774502800c47d464065694e26291ce0
Megan D <45672063+megan-d@users.noreply.github.com>
2021-01-08 14:00:08 -0700

    Remove demo and join buttons from landing if authenticated

--- client/src/components/views/Landing.js
+++ client/src/components/views/Landing.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Runner from '../../assets/images/lady-running-cover.svg';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
   //Need to figure out how to not display footer on landing page
   return (
     <div className='main-content'>
@@ -14,17 +16,22 @@ const Landing = () => {
             Activity and fitness tracking to help you view your progress and
             reach your fitness goals
           </h2>
-          <div className='landing-buttons'>
-            <div className='button landing-button'>
-              <a href='/demo' className='landing-link'>Demo</a>
+          {isAuthenticated ? (
+            ''
+          ) : (
+            <div className='landing-buttons'>
+              <div className='button landing-button'>
+                <a href='/demo' className='landing-link'>
+                  Demo
+                </a>
+              </div>
+              <div className='button landing-button'>
+                <a href='/register' className='landing-link'>
+                  Join Now
+                </a>
+              </div>
             </div>
-            <div className='button landing-button'>
-              <a href='/register' className='landing-link'>
-                Join Now
-              </a>
-            </div>
-          </div>
-
+          )}
         </div>
 
         <div className='image-container'>
@@ -35,4 +42,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
