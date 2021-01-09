57f5468c9eb4b0bb2fe874f747e6ccc19c53b72b
Megan D <45672063+megan-d@users.noreply.github.com>
2021-01-09 15:20:11 -0700

    Modify db connection for heroku deploy

--- db.js
+++ db.js
@@ -13,18 +13,18 @@ const Pool = require('pg').Pool;
 const Client = require('pg').Client;
 
 //THIS IS THE CODE TO USE FOR LOCAL TESTING CONNECTED TO HEROKU POSTGRES DB
-const pool = new Pool({
-  connectionString:
-    'postgres://gjbnsjqkpebvfb:ed80cb007c6645d7fee8dbd046afc6258c49344945d76eeed17a5e0570cc7cb5@ec2-3-231-241-17.compute-1.amazonaws.com:5432/d9uae9l4hf5gms',
-  host: process.env.HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PS,
-  port: 5432,
-  database: process.env.PG_DATABASE,
-    ssl: {
-            rejectUnauthorized: false
-          }
-});
+// const pool = new Pool({
+//   connectionString:
+//     'postgres://gjbnsjqkpebvfb:ed80cb007c6645d7fee8dbd046afc6258c49344945d76eeed17a5e0570cc7cb5@ec2-3-231-241-17.compute-1.amazonaws.com:5432/d9uae9l4hf5gms',
+//   host: process.env.HOST,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PS,
+//   port: 5432,
+//   database: process.env.PG_DATABASE,
+//     ssl: {
+//             rejectUnauthorized: false
+//           }
+// });
 
 // const client = new Client({
 //   connectionString:
@@ -40,15 +40,15 @@ const pool = new Pool({
 // });
 
 // pool.connect();
-console.log('connected to DB!');
+// console.log('connected to DB!');
 
-//THIS IS THE CODE TO USE FOR HEROKU DEPLOY
-// const pool = new Pool({
-// connectionString: process.env.DATABASE_URL,
-// //   ssl: {
-// //           rejectUnauthorized: true
-// //         }
-// });
+// THIS IS THE CODE TO USE FOR HEROKU DEPLOY
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//           rejectUnauthorized: true
+//         }
+});
 
 // const client = new Client({
 // connectionString: process.env.DATABASE_URL,
